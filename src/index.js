101440deffd13af1c544f090cd14c2c69cc55a99
ongimkshp <doducmanh.bb@gmail.com>
2022-05-11 16:24:14 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -45,12 +45,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-
 app.set('views', path.join(__dirname, 'resources', 'views'));
+
 
 route(app);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
-
